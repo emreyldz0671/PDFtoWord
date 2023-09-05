0e8d0d26d2396d6c87427ef14fc9f0e5cd87cdf7
Emre Yıldız <emreeyldz06@gmail.com>
2023-09-05 12:54:31 +0300

    PDFtoWord added

---
+++ main.py
@@ -0,0 +1,14 @@
+from pdf2docx import parse
+
+parse(pdf_file="MüzikEtkinlikleri.pdf", docx_file="Muzik.docx", start=0, end=None)
+
+#Bir diğer yöntem:
+
+#from pdf2docx import Converter
+
+#pdf_path = "sample.pdf"
+#docx_path = "sample.docx"
+
+#cv = Converter(pdf_file=pdf_path)
+#cv.Converter(docx_filename=docx.path)
+#cv.close()
